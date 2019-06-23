d79c13355440f342afafccb806f312cbdeef79c9
chaojen.chen@gmail.com <chaojen.chen@gmail.com>
2019-06-23 16:56:19 +0800

    add ex2

--- tests/ex2/test_sample_2.py
+++ tests/ex2/test_sample_2.py
@@ -2,9 +2,9 @@ import my_app
 
 def test_case_1():
     lst_input = "Ironman"
-    expected = "Iro"
+    not_expected = ""
     
-    assert my_app.grab_first_three(lst_input) == expected
+    assert my_app.grab_first_three(lst_input) != not_expected
     
 def test_case_2():
     lst_input = "ab"
