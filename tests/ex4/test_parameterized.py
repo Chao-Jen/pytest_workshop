23758c61d6403d4035059d330215d59a98fd96af
chaojen.chen@gmail.com <chaojen.chen@gmail.com>
2019-06-26 22:05:36 +0800

    update ex4

--- tests/ex4/test_parameterized.py
+++ tests/ex4/test_parameterized.py
@@ -1,9 +1,11 @@
 import pytest
 from my_calculator import division
 
+
 @pytest.mark.parametrize("a", [1, 2, 3, 4])
 def test_division_param(a):
     assert division(a, 1) == a
+    
     
 @pytest.mark.parametrize("a,b,expected", [(4,2,2), (9,3,3)])
 def test_division_param_2(a, b, expected):
